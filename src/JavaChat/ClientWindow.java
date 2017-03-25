70cfeaf89c21d9e813c551eec495d82c9446af1c
Home <chhavi.alok25@gmail.com>
2017-03-25 11:30:13 +0530

    new edits

--- src/JavaChat/ClientWindow.java
+++ src/JavaChat/ClientWindow.java
@@ -17,8 +17,15 @@ import javax.swing.text.DefaultCaret;
 import javax.swing.JFrame;
 import JavaChat.Client;
 import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -36,30 +43,120 @@ public class ClientWindow extends JFrame implements Runnable{
     private Thread listen,run;
     private boolean running=false;
     public ClientWindow(String name,String address,int port) {
-        client=new Client(name,address,port);
-        
-        boolean connect=client.openConnection(address);
-        if(!connect){
-            System.err.println("connection failed!");
-            console("connection failed!");
-        }
-        
-     
-        initComponents();
-        
-        myInitComponents();
-        
-        console("Attempting a connection to "+address+": "+port+" ,user: "+name);
-        String connectiion="/c/"+name+"/e/";
-        client.send(connectiion.getBytes());
-         running=true;
-        run=new Thread(this,"Running");
-        run.start();
+       setTitle("Cherno Chat Client");
+		client = new Client(name, address, port);
+		boolean connect = client.openConnection(address);
+		if (!connect) {
+			System.err.println("Connection failed!");
+			console("Connection failed!"); 
+		} 
+		createWindow(); 
+		console("Attempting a connection to " + address + ":" + port + ", user: " + name);
+		String connection = "/c/" + name + "/e/";
+		client.send(connection.getBytes());
+		
+		running = true;
+		run = new Thread(this, "Running");
+		run.start();
        
     }
     /* recieving data from server
     
     */
+    private void createWindow() { 
+		try { 
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		} 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(880, 550);
+		setLocationRelativeTo(null);
+ 
+		
+ 
+		//mntmExit = new JMenuItem("Exit");
+		//mnFile.add(mntmExit);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+ 
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 28, 815, 30, 7 }; // SUM = 880
+		gbl_contentPane.rowHeights = new int[] { 25, 485, 40 }; // SUM = 550
+		contentPane.setLayout(gbl_contentPane);
+ 
+		txtHistory = new JTextArea();
+		txtHistory.setEditable(false);
+		JScrollPane scroll = new JScrollPane(txtHistory);
+		caret = (DefaultCaret) txtHistory.getCaret();
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+		GridBagConstraints scrollConstraints = new GridBagConstraints();
+		scrollConstraints.insets = new Insets(0, 0, 5, 5);
+		scrollConstraints.fill = GridBagConstraints.BOTH;
+		scrollConstraints.gridx = 0;
+		scrollConstraints.gridy = 0;
+		scrollConstraints.gridwidth = 3;
+		scrollConstraints.gridheight = 2;
+		scrollConstraints.weightx = 1;
+		scrollConstraints.weighty = 1;
+		scrollConstraints.insets = new Insets(0, 5, 0, 0);
+		contentPane.add(scroll, scrollConstraints);
+ 
+		txtMessage = new JTextField();
+		txtMessage.addKeyListener(new KeyAdapter() {
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					send(txtMessage.getText(), true);
+				} 
+			} 
+		}); 
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 0;
+		gbc_txtMessage.gridy = 2;
+		gbc_txtMessage.gridwidth = 2;
+		gbc_txtMessage.weightx = 1;
+		gbc_txtMessage.weighty = 0;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+                txtMessage.addKeyListener(new java.awt.event.KeyAdapter() {
+            @Override
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtMessageKeyPressed(evt);
+            }
+        });
+ 
+		btnSend = new JButton("Send");
+		  btnSend.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSendActionPerformed(evt);
+            }
+        });
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.insets = new Insets(0, 0, 0, 5);
+		gbc_btnSend.gridx = 2;
+		gbc_btnSend.gridy = 2;
+		gbc_btnSend.weightx = 0;
+		gbc_btnSend.weighty = 0;
+		contentPane.add(btnSend, gbc_btnSend);
+ 
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				String disconnect = "/d/" + client.getID() + "/e/";
+				send(disconnect, false);
+				running = false;
+				client.close();
+			} 
+		}); 
+ 
+		setVisible(true);
+ 
+		txtMessage.requestFocusInWindow();
+	} 
+ 
    
 
     /**
@@ -134,60 +231,7 @@ public class ClientWindow extends JFrame implements Runnable{
      client.send(message.getBytes());
        
 }
-    private void myInitComponents() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-        setLocationRelativeTo(null);
-        setSize(880,550);
-        setTitle("Chat Client");
-        //for main grid layout
-        GridBagLayout gbl=new GridBagLayout();
-        gbl.columnWidths=new int[]{28,815,30,7};//sum-880
-        gbl.rowHeights=new int[]{35,475,40};//sum-550
-        gbl.columnWeights=new double[]{1,0,Double.MIN_VALUE};
-        gbl.rowWeights=new double[]{1,0,Double.MIN_VALUE};
-        getContentPane().setLayout(gbl);
-        //for chat text area
-         JScrollPane scroll=new JScrollPane(txtHistory);
-        caret=(DefaultCaret)txtHistory.getCaret();
-        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-        GridBagConstraints scrollConstraints=new GridBagConstraints();
-        scrollConstraints.insets=new Insets(0,0,5,5);
-        scrollConstraints.fill=GridBagConstraints.BOTH;
-        scrollConstraints.gridx=0;
-        scrollConstraints.gridy=0;
-        scrollConstraints.gridwidth=3;
-        scrollConstraints.gridheight=2;
-        scrollConstraints.insets=new Insets(0,5,0,0);
-        getContentPane().add(scroll,scrollConstraints);
-        //for send button
-        GridBagConstraints agbc=new GridBagConstraints();
-        agbc.insets=new Insets(0,0,0,5);
-        agbc.gridx=2;
-        agbc.gridy=2;
-        getContentPane().add(btnSend,agbc);
-        addWindowListener(new WindowAdapter(){
-            @Override
-            public void windowClosing(WindowEvent e){
-                String disconnect="/d/"+client.getID()+"/e/";
-                send(disconnect,false);
-                client.close();
-            } 
-    });
-        //for text message
-        GridBagConstraints bgbc=new GridBagConstraints();
-        bgbc.insets=new Insets(0,0,0,5);
-        bgbc.fill=GridBagConstraints.HORIZONTAL;
-        bgbc.gridx=0;
-        bgbc.gridy=2;
-        bgbc.gridwidth=2;
-        getContentPane().add(txtMessage,bgbc);
-        txtMessage.requestFocusInWindow();
-           setVisible(true);
-    }
+    
     
     public void console(String message){
          System.out.println("ClientWindow.java--- console");
@@ -206,7 +250,8 @@ public class ClientWindow extends JFrame implements Runnable{
              console("Successfully connected to server! ID: "+client.getID());
           }else if(message.startsWith("/m/"))
           {
-              String text=message.split("/m/|/e/")[1];
+              String text= message.substring(3);
+             text= text.split("/e/")[0];
               System.out.println("text: "+text);
               console(text);
           }
@@ -227,7 +272,7 @@ public class ClientWindow extends JFrame implements Runnable{
     private javax.swing.JTextArea txtHistory;
     private javax.swing.JTextField txtMessage;
     // End of variables declaration//GEN-END:variables
-
+private JPanel contentPane;
     @Override
     public void run() {
         listen();
